db880f6e2b76bf932c5f6be85d5968654a7f0a7f
Jona <mr-jona@hotmail.com>
2022-05-20 17:18:43 +0200

    break

---
+++ README.txt
@@ -0,0 +1,3 @@
+to do list for the odin project
+
+live version: https://atubak.github.io/to-do-list/
--- src/index.js
+++ src/index.js
@@ -1,9 +1,63 @@
 import "./style.css";
 
-const ProjectFactory = (name) => {
-    console.log(name)
+const ProjectFactory = () => {
+    let name = "Untitled";
+    let toDoItems = [];
+
+    function changeName(newName) {
+        return this.name = newName;
+        // reloadProjectObjectInDom()
+    }
+
+    function addItem() {
+        const newToDo = toDoObjectFactory();
+        return this.toDoItems.push(newToDo);
+        // reloadProjectObjectInDom()
+    }
+
+    function removeItem(itemToBeRemovedIndex) {
+        return this.toDoItems.splice(itemToBeRemovedIndex, 1);
+        // reloadProjectObjectInDom()
+    }
+
+    return {name, toDoItems, changeName, addItem, removeItem};
+};
+
+const toDoObjectFactory = () => {
+    let title = "Untitled";
+    let description = "Empty";
+    let dueDate;
+    let priority;
+
+    function changeTitle(newTitle) {
+        return this.title = newTitle;
+        // reloadItemObjectInDom()
+    }
+
+    function changeDescription(newDescription) {
+        return this.description = newDescription;
+        // reloadItemObjectInDom()
+    }
+
+    function changeDueDate(newDate) {
+        return this.dueDate = newDate;
+        // reloadItemObjectInDom()
+    }
+
+    function changePriority(newPriority) {
+        return this.priority = newPriority;
+        // reloadItemObjectInDom()
+    }
+
+    return {title, description, dueDate, priority, changeTitle, changeDescription, changeDueDate, changePriority};
 };
 
 
-ProjectFactory("boi");
-console.log("howdysssss");
+
+
+
+const firstProject = ProjectFactory();
+firstProject.changeName("jonatan");
+firstProject.addItem();
+console.log(firstProject);
+
