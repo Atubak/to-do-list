eab36b983a80d4cae14b840e6b109e863a9effa3
Jona <mr-jona@hotmail.com>
2022-05-23 12:30:35 +0200

    made header

--- src/index.js
+++ src/index.js
@@ -8,12 +8,24 @@ import { ToDoObjectFactory } from "./toDoObject";
 
 
 
-projectList.add(ProjectFactory("jona"));
-projectList.add(ProjectFactory("booba"));
-projectList.add(ProjectFactory("vegana"));
+// projectList.add(ProjectFactory("jona"));
+// projectList.add(ProjectFactory("booba"));
+// projectList.add(ProjectFactory("vegana"));
 
-projectList.list[0].addItem(ToDoObjectFactory());
-projectList.list[0].addItem(ToDoObjectFactory());
-projectList.list[0].removeItem(0);
+// projectList.list[0].addItem(ToDoObjectFactory());
+// projectList.list[0].addItem(ToDoObjectFactory());
+// projectList.list[0].removeItem(0);
 
-console.log(projectList);
+// console.log(projectList);
+
+function HtmlLoader() {
+const body = document.querySelector("body");
+body.innerHTML = `
+<header>
+  <div id="logo">𝕋</div>
+  <div id="title">To Do List</div>
+  <div id="plus">+</div>
+</header>`;
+};
+
+HtmlLoader();
